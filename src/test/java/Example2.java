ad4df6d5f2dce325539189be095d3368b14a63ea
sreejyothi <sreejyothi@qburst.com>
2023-03-27 19:12:38 +0530

    addedd

---
+++ src/test/java/Example2.java
@@ -0,0 +1,40 @@
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+
+import io.percy.selenium.Percy;
+
+
+
+public class Example2 {
+	
+	@Test
+	public void getstring()
+	{
+		
+		 ChromeOptions chromeOptions =new ChromeOptions();
+		    chromeOptions.addArguments("--remote-allow-origins=*");
+		    WebDriver driver =new ChromeDriver(chromeOptions);
+		    Percy percy = new Percy(driver);
+		    driver.get("https://www.saucedemo.com/");
+		    percy.snapshot("saucedemopage");
+	  }
+	
+	@Test
+	public void getUrl()
+	{
+		
+		 ChromeOptions chromeOptions =new ChromeOptions();
+		    chromeOptions.addArguments("--remote-allow-origins=*");
+		    WebDriver driver =new ChromeDriver(chromeOptions);
+		    Percy percy = new Percy(driver);
+		    driver.get("https://www.tesla.com/");
+		    System.out.println(driver.getCurrentUrl());
+		    percy.snapshot("teslapage");
+	  }
+	
+}
---
+++ src/test/java/Pipelinescript/Example.java
@@ -0,0 +1,41 @@
+package Pipelinescript;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+
+import io.percy.selenium.Percy;
+
+
+
+public class Example {
+	
+	@Test
+	public void getstring()
+	{
+		
+		 ChromeOptions chromeOptions =new ChromeOptions();
+		    chromeOptions.addArguments("--remote-allow-origins=*");
+		    WebDriver driver =new ChromeDriver(chromeOptions);
+		  
+		    driver.get("https://www.saucedemo.com/");
+		   
+	  }
+	
+	@Test
+	public void getUrl()
+	{
+		
+		 ChromeOptions chromeOptions =new ChromeOptions();
+		    chromeOptions.addArguments("--remote-allow-origins=*");
+		    WebDriver driver =new ChromeDriver(chromeOptions);
+		   
+		    driver.get("https://www.tesla.com/");
+		    System.out.println(driver.getCurrentUrl());
+		    
+	  }
+	
+}
